16e22a4a74b54b060fd1bcefc00397122ece8d9f
SMTanimur <smtanimurrahman404@gmail.com>
2023-12-07 09:03:14 +0600

    feat: :hammer: createAHelloFn and counterFn solve problem

---
+++ createHelloWorldFn.ts
@@ -0,0 +1,23 @@
+/**
+ * 
+ *function createHelloWorld() {
+    
+	return function(...args): string {
+        return "Hello World"
+    };
+};
+ */
+
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+function createHelloWorld() {
+    
+	return function(...args): string {
+        return "Hello World"
+    };
+};
